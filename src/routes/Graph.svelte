1b844cc99ac37a2f34a0249e2f762e851e90dce4
7nik <kifiranet@gmail.com>
2022-01-12 10:43:07 +0200

    add animations

--- src/routes/Graph.svelte
+++ src/routes/Graph.svelte
@@ -3,17 +3,32 @@
 	export const ssr = false;
 </script>
 <script lang="ts">
-	let n = 6;
-	let matrix: {enabled:boolean, w:number}[][] = [], pos: {i:number, x:number, y:number }[];
-	let M = 100, S1 = 80, S2 = 90, S3 = 100;
-	
-	function newJoint() { return { enabled: !Math.round(Math.random()+0.1), w: Math.round(Math.random()*100) }; }
-	function newJoints(n:number) { return Array.from({ length:n }, newJoint); }
-	// a,b = 0..n-1; a<=b
-	function color(a:number,b:number) { return `hsl(${Math.round(360*(a*n - a*(a-1)/2 + b-a)/(n*(n+1)/2))}, 100%, 50%)`; }
-	function mid(a:number,b:number,p:string) { return (pos[a][p]*(a%2?9:11) + pos[b][p]*(a%2?11:9))/20; }
+	import { fade } from "svelte/transition";
+	import { linear } from "svelte/easing";
+	import { tweened } from "svelte/motion";
 
-	function off(a:number,b:number) {
+	type Point = {i:number, x:number, y:number };
+	const M = 100, S1 = 80, S2 = 90, S3 = 100;
+	const duration = 150;
+	let n = tweened(6, { duration, easing: linear });
+	let matrix: {enabled:boolean, w:number}[][] = [], pos: Point[];
+	
+	function newJoint() {
+		return { enabled: !Math.round(Math.random()+0.1), w: Math.round(Math.random()*100) }; 
+	}
+	function newJoints(n:number) { 
+		return Array.from({ length:n }, newJoint); 
+	}
+	// a,b = 0..n-1; a<=b<n
+	function color(a:number, b:number, n:number) { 
+		const hue = Math.round(360*(a*n - a*(a-1)/2 + b-a) / (n*(n+1)/2));
+		return `hsl(${hue}, 100%, 50%)`; 
+	}
+	function mid(p1: Point, p2: Point, ax:string) { 
+		return (p1?.[ax]*(p1?.i%2?9:11) + p2?.[ax]*(p1?.i%2?11:9))/20; 
+	}
+
+	function remove(a:number,b:number) {
 		matrix[a][b].enabled = false;
 	}
 
@@ -80,29 +95,57 @@
 		}
 	}
 
+	function transLine (node:Element, { duration, easing, i1, i2 }: { duration: number, easing: (x:number)=>number, i1:number, i2:number }) {
+		return { 
+			duration,
+			easing,
+			tick (t: number) {
+				const point1 = `${pos[i1]?.x},${pos[i1]?.y}`;
+				const point2 = `${pos[i2]?.x},${pos[i2]?.y}`;
+				node.setAttribute("points", `${point1} ${point2}`);
+				(node as HTMLElement).style.opacity = t.toString();
+			}
+		};
+	}
+
+	function transCircle (node:Element, { duration, easing, scale: size, i }: { duration: number, easing: (x:number)=>number, scale: number, i: number }) {
+		const names = node.nodeName === "text" ? ["x", "y"] : ["cx", "cy"];
+		return { 
+			duration,
+			easing,
+			tick (t: number) {
+				const { x, y } = pos[i] ?? {};
+				node.setAttribute(names[0], (M+x*size).toString());
+				node.setAttribute(names[1], (M+y*size).toString());
+				(node as HTMLElement).style.opacity = t.toString();
+			}
+		};
+	}
+	let nn = $n;
+	$: $n = nn;
 	$: {
-		if (matrix.length < n) {
+		if (matrix.length < nn) {
 			matrix = [
-				...matrix.map(row => [...row, ...newJoints(n-row.length)]),
-				...Array.from({ length:n-matrix.length }, () => newJoints(n)),
+				...matrix.map(row => [...row, ...newJoints(nn-row.length)]),
+				...Array.from({ length:nn-matrix.length }, () => newJoints(nn)),
 			];
-		} else if (n < matrix.length) {
-			matrix = matrix.slice(0, n).map(row => row.slice(0, n));
+		} else if (nn < matrix.length) {
+			matrix = matrix.slice(0, nn).map(row => row.slice(0, nn));
 		}
 	}
-	$: pos = Array.from({ length:n }, (_, i) => ({
+	$: pos = Array.from({ length:$n+1 }, (_, i) => ({
 		i: i,
-		x: Math.cos(i*2*Math.PI/n - Math.PI/2),
-		y: Math.sin(i*2*Math.PI/n - Math.PI/2),
+		x: Math.cos(i*2*Math.PI/$n - Math.PI/2),
+		y: Math.sin(i*2*Math.PI/$n - Math.PI/2),
 	}));
 </script>
 
 Кількість вершин: 
-<input type="number" min="1" max="15" bind:value={n} > <br>
+<input type="number" min="1" max="15" bind:value={nn} > <br>
 
-<div class="matrix" style="--size: {n}">
+<div class="matrix" style="--size: {nn}">
 	<div></div>
-	{#each {length:n} as _,i}<div class="header">{i+1}</div>{/each}
+	{#each {length:nn} as _,i}<div class="header">{i+1}</div>{/each}
 	
 	{#each matrix as row, i}
 		<div class="header">{i+1}</div>
@@ -123,7 +166,6 @@
 	{/each}
 </div>
 
-{#key n}
 <svg width="600" height="600" viewBox="-10 -10 220 220"
      xmlns="http://www.w3.org/2000/svg"
 	 class:grab={!!dragging}
@@ -134,21 +176,37 @@
 		{#each row as { enabled,w }, i2}
 			{#if enabled && i2>=i1}
 				{#if i1==i2}
-					<circle cx="{M+pos[i1].x*S2}" 
-                        cy="{M+pos[i1].y*S2}" 
+					<circle
+						in:fade={{ duration, easing:linear }}
+						out:transCircle={{ 
+							duration,
+							easing: linear,
+							scale: S2,
+							i: i1,
+						}}
+						cx={M+pos[i1]?.x*S2} 
+						cy={M+pos[i1]?.y*S2}
                         r="10" 
                         fill="none"
-                        stroke="{color(i1,i2)}" 
+                        stroke="{color(i1, i2, $n)}" 
                         stroke-width={(w+5)/20}
-						on:click={() => off(i1,i2)}
+						on:click={() => remove(i1,i2)}
 						class="off"
 					/>
 				{:else}
-					<polyline points="{pos[i1].x},{pos[i1].y} {pos[i2].x},{pos[i2].y}"
-                        transform="translate({M}, {M}) scale({S1})"
-                        fill="none" stroke="{color(i1,i2)}" 
-                        stroke-width={(w+10)/20/S1}
-						on:click={() => off(i1,i2)}
+					<polyline 
+						in:fade={{ duration }}
+						out:transLine={{ 
+							duration,
+							easing: linear, 
+							i1,
+							i2,
+						}}
+						points={`${pos[i1]?.x},${pos[i1]?.y} ${pos[i2]?.x},${pos[i2]?.y}`}
+						transform="translate({M}, {M}) scale({S1})"
+						fill="none" stroke="{color(i1, i2, $n)}" 
+						stroke-width={(w+10)/20/S1}
+						on:click={() => remove(i1,i2)}
 						class="off"
 					/>
 				{/if}
@@ -161,17 +219,33 @@
 		{#each row as { enabled,w }, i2}
 			{#if enabled && i2>=i1}
 				{#if i1==i2}
-					<text x="{M+pos[i1].x*S3}" 
-                        y="{M+pos[i1].y*S3}" 
+					<text 
+						in:fade={{duration}}
+						out:transCircle={{ 
+							duration,
+							easing: linear,
+							scale: S3,
+							i:i1
+						}}
+						x={M+pos[i1]?.x*S3}
+						y={M+pos[i1]?.y*S3}
                         font-size="7" 
                         font-weight="bold"
                         text-anchor="middle" 
                         alignment-baseline="middle"
-                        stroke="{color(i1,i2)}"
+                        stroke="{color(i1, i2, $n)}"
                         fill="white"
 					>{+w}</text>
-					<text x="{M+pos[i1].x*S3}" 
-                        y="{M+pos[i1].y*S3}" 
+					<text 
+						in:fade={{duration}}
+						out:transCircle={{ 
+							duration,
+							easing: linear,
+							scale: S3,
+							i:i1
+						}}
+						x={M+pos[i1]?.x*S3}
+						y={M+pos[i1]?.y*S3}
                         font-size="7" 
                         font-weight="bold"
                         text-anchor="middle" 
@@ -181,17 +255,21 @@
 						class="resizable"
 					>{+w}</text>
 				{:else}
-					<text x="{M+mid(i1,i2,'x')*S1}" 
-                        y="{M+mid(i1,i2,'y')*S1}" 
+					<text 
+						transition:fade={{duration}}
+						x={M+mid(pos[i1], pos[i2], 'x')*S1} 
+						y={M+mid(pos[i1], pos[i2], 'y')*S1}
                         font-size="7" 
                         font-weight="bold"
                         text-anchor="middle" 
                         alignment-baseline="middle"
-                        stroke="{color(i1,i2)}"
+                        stroke="{color(i1, i2, $n)}"
                         fill="white"
 					>{+w}</text>
-					<text x="{M+mid(i1,i2,'x')*S1}" 
-                        y="{M+mid(i1,i2,'y')*S1}" 
+					<text 
+						transition:fade={{duration}}
+						x={M+mid(pos[i1], pos[i2], 'x')*S1} 
+						y={M+mid(pos[i1], pos[i2], 'y')*S1}
                         font-size="7" 
                         font-weight="bold"
                         text-anchor="middle" 
@@ -213,24 +291,41 @@
 	
 	<!-- points -->
 	{#each pos as { i, x, y }}
-  	<circle cx="{M+x*S1}" 
-        cy="{M+y*S1}" 
-        r="6" 
-        fill="black"
-		use:draggable={i} 
-		class="grabable"
-	/>
-  	<text x="{M+x*S1}" 
-        y="{M+y*S1}" 
-        font-size="7" 
-        font-weight="bold"
-        text-anchor="middle" 
-        alignment-baseline="middle"
-        fill="white"
-    >{i+1}</text>
+		{#if i < nn }
+		<circle 
+			in:fade={{duration, easing: linear}}
+			out:transCircle={{ 
+				duration,
+				easing: linear,
+				scale: S1,
+				i
+			}}
+			cx={M+x*S1}
+			cy={M+y*S1}
+			r="6" 
+			fill="black"
+			use:draggable={i} 
+			class="grabable"
+		/>
+		<text 
+			in:fade={{duration, easing: linear}}
+			out:transCircle={{ 
+				duration,
+				easing: linear,
+				scale: S1,
+				i
+			}}
+			x={M+x*S1}
+			y={M+y*S1}
+			font-size="7" 
+			font-weight="bold"
+			text-anchor="middle" 
+			alignment-baseline="middle"
+			fill="white"
+		>{i+1}</text>
+		{/if}
 	{/each}
 </svg>
-{/key}
 
 <style>
 	.matrix {
