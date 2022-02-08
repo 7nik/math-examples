1d2e54acc5bd5b16e448379b2b78d5f2a6d45416
7nik <kifiranet@gmail.com>
2022-02-09 01:08:51 +0200

    Updates

---
+++ _app/pages/index.svelte-f48ad8aa.js
@@ -0,0 +1,1 @@
+import{S as o,i as r,s as c,t as i,g as d,f as l,T as e,d as u}from"../chunks/vendor-836a0c06.js";function f(s){let t;return{c(){t=i("Nan des ka?")},l(a){t=d(a,"Nan des ka?")},m(a,n){l(a,t,n)},p:e,i:e,o:e,d(a){a&&u(t)}}}class p extends o{constructor(t){super();r(this,t,null,f,c,{})}}export{p as default};
